c442061f597199a759ae980df6b0e8b2a0e8fbe4
Eugene O'Neil <ejo7220@gmail.com>
2020-07-31 16:47:50 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+consoles = {"console1" => "Xbox", "console2" => "Playstation", "console3" => "Switch"}
 
   
 end
